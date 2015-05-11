8f6113656b0c9d2fae5bd3d466e8be417734acb0
SviMik <svimik@mail.ru>
2015-05-11 23:31:49 +0300

    ponyNamesDecorator plugin refactoring

--- etherpad/src/plugins/ponyNamesDecorator/static/js/main.js
+++ etherpad/src/plugins/ponyNamesDecorator/static/js/main.js
@@ -1,39 +1,30 @@
 function ponyNamesDecoratorInit() {
-	this.hooks = ['aceCreateDomLine', 'aceGetFilterStack'];
-	this.aceCreateDomLine = aceCreateDomLine;
+	this.hooks = ['aceInitInnerdocbodyHead', 'aceGetFilterStack'];
+	this.aceInitInnerdocbodyHead = aceInitInnerdocbodyHead;
 	this.aceGetFilterStack = aceGetFilterStack;
 }
 
-var regexpNames = /(Applejack|Fluttershy|Pinkie( *Pie)?|Rarity|Rainbow( *Dash)?|Twilight( *Sparkle)?|Spike|Celestia|Luna|Discord|Shining_Armor|Princess_Cadence|Scootaloo|SweetieBelle|AppleBloom|BabsSeed|Trixie|Zecora|Snips|Snails|MrCake|MrsCake|Granny Smith|Big Mac|Cheerilee|Reference|Somepony):/gi;
-var colors = {
-	applejack: 'hsl(036, 100%, 71%)',
-	fluttershy: 'hsl(055, 94%, 82%)',
-	pinkiepie: 'hsl(335, 88%, 84%)',
-	pinkie: 'hsl(335, 88%, 84%)',
-	rarity: 'hsl(240, 8%, 95%)',
-	rainbowdash: 'hsl(195, 100%, 81%)',
-	rainbow: 'hsl(195, 100%, 81%)',
-	twilightsparkle: 'hsl(283, 65%, 81%)',
-	twilight: 'hsl(283, 65%, 81%)',
-};
+function aceInitInnerdocbodyHead(args) {
+	args.iframeHTML.push('\'<link rel="stylesheet" type="text/css" href="/static/css/plugins/ponyNamesDecorator/pad.css"/>\'');
+}
 
-function getRegexpFilter(linestylefilter, regExp, tag) {
-	// This function is a modified version of linestylefilter.getRegexpFilter() from Etherpad's linestylefilter.js
+function ponynames(linestylefilter){
 	return function (lineText, textAndClassFunc) {
+		var regExp = /^([^a-z]{0,16})(([a-zA-Z0-9]+ *){1,3}:)/gi;
 		regExp.lastIndex = 0;
 		var splitPoints = [];
 		var execResult;
-		while(execResult = regExp.exec(lineText))
-			splitPoints.push(execResult.index, execResult.index + execResult[0].length);
-		if(splitPoints.length==0) 
+		if(execResult = regExp.exec(lineText)){
+			var from = execResult.index + execResult[1].length;
+			var to = from+execResult[2].length;
+			splitPoints.push(from, to);
+		}else{
 			return textAndClassFunc;
+		}
 
 		function regExpMatchForIndex(idx) {
-			for(var k=0; k<splitPoints.length; k+=2) {
-				var startIndex = splitPoints[k];
-				var endIndex = splitPoints[k+1];
-				if(idx >= startIndex && idx < endIndex)
-					return lineText.substring(startIndex, endIndex);
+			if(idx >= splitPoints[0] && idx < splitPoints[1]){
+				return lineText.substring(splitPoints[0], splitPoints[1]);
 			}
 			return false;
 		}
@@ -42,11 +33,11 @@ function getRegexpFilter(linestylefilter, regExp, tag) {
 			var curIndex = 0;
 			return function(txt, cls) {
 				var txtlen = txt.length;
-				var newCls = cls;
-				var regExpMatch = regExpMatchForIndex(curIndex);
-				if(regExpMatch)
-					newCls += " " + tag + ":" + regExpMatch.replace(/\s|:/g, '').toLowerCase();
-				textAndClassFunc(txt, newCls);
+				var regExpMatch;
+				if(regExpMatch = regExpMatchForIndex(curIndex)){
+					cls += (cls==""?"":" ")+"pony_name pony_" + regExpMatch.replace(/\s|:/g, '').toLowerCase();
+				}
+				textAndClassFunc(txt, cls);
 				curIndex += txtlen;
 			};
 		})();
@@ -56,25 +47,7 @@ function getRegexpFilter(linestylefilter, regExp, tag) {
 }
 
 function aceGetFilterStack(args) {
-	return [getRegexpFilter(args.linestylefilter, regexpNames, 'CharacterName')];
-}
-
-function aceCreateDomLine(args) {
-	if(args.cls.indexOf('CharacterName:') > -1) {
-		var color;
-		cls = args.cls.replace(/(^| )CharacterName:(\S+)/g, function(whole, space, name) {
-			color = colors[name];
-			return '';
-		});
-		var extraOpenTags = '<span style="font-size:12px; background-color: #fdb; font-weight: bold;">';
-		if(color)
-			extraOpenTags = '<span style="font-size:12px; background-color: ' + color + '; font-weight: bold;">';
-		return [{
-			cls: cls,
-			extraOpenTags: extraOpenTags,
-			extraCloseTags: '</span>'
-		}];
-	}
+	return [ponynames(args.linestylefilter)];
 }
 
 /* used on the client side only */
