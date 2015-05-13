9d86149719a495756822cd26cac6860e78b6a86c
SviMik <svimik@mail.ru>
2015-05-13 03:43:21 +0300

    ponyNamesDecorator - Don't spoil global namespace

--- etherpad/src/plugins/ponyNamesDecorator/static/js/main.js
+++ etherpad/src/plugins/ponyNamesDecorator/static/js/main.js
@@ -1,50 +1,48 @@
 function ponyNamesDecoratorInit() {
 	this.hooks = ['aceInitInnerdocbodyHead', 'aceGetFilterStack'];
-	this.aceInitInnerdocbodyHead = aceInitInnerdocbodyHead;
-	this.aceGetFilterStack = aceGetFilterStack;
-}
 
-function aceInitInnerdocbodyHead(args) {
-	args.iframeHTML.push('\'<link rel="stylesheet" type="text/css" href="/static/css/plugins/ponyNamesDecorator/pad.css"/>\'');
-}
+	this.aceInitInnerdocbodyHead = function(args) {
+		args.iframeHTML.push('\'<link rel="stylesheet" type="text/css" href="/static/css/plugins/ponyNamesDecorator/pad.css"/>\'');
+	}
 
-function ponynames(linestylefilter){
-	return function (lineText, textAndClassFunc) {
-		var regExp = /^([^a-z]{0,18})(([a-z][a-z0-9]* *){1,3}:)/gi;
-		regExp.lastIndex = 0;
-		var splitPoints = [];
-		var execResult;
-		if(execResult = regExp.exec(lineText)){
-			var from = execResult.index + execResult[1].length;
-			var to = from+execResult[2].length;
-			splitPoints.push(from, to);
-		}else{
-			return textAndClassFunc;
-		}
+	function ponynames(linestylefilter){
+		return function (lineText, textAndClassFunc) {
+			var regExp = /^([^a-z]{0,18})(([a-z][a-z0-9]* *){1,3}:)/gi;
+			regExp.lastIndex = 0;
+			var splitPoints = [];
+			var execResult;
+			if(execResult = regExp.exec(lineText)){
+				var from = execResult.index + execResult[1].length;
+				var to = from+execResult[2].length;
+				splitPoints.push(from, to);
+			}else{
+				return textAndClassFunc;
+			}
 
-		var handleRegExpMatchsAfterSplit = (function() {
-			var curIndex = 0;
-			return function(txt, cls) {
-				var txtlen = txt.length;
-				if(curIndex >= splitPoints[0] && curIndex < splitPoints[1]){
-					if(lineText.substring(splitPoints[1], splitPoints[1]+2)!="//"){ // prevents links from highlighting
-						var classname=lineText.substring(splitPoints[0], splitPoints[1]).replace(/\s|:/g, '').toLowerCase();
-						cls += (cls==""?"":" ")+"pony_name pony_" + classname;
+			var handleRegExpMatchsAfterSplit = (function() {
+				var curIndex = 0;
+				return function(txt, cls) {
+					var txtlen = txt.length;
+					if(curIndex >= splitPoints[0] && curIndex < splitPoints[1]){
+						if(lineText.substring(splitPoints[1], splitPoints[1]+2)!="//"){ // prevents links from highlighting
+							var classname=lineText.substring(splitPoints[0], splitPoints[1]).replace(/\s|:/g, '').toLowerCase();
+							cls += (cls==""?"":" ")+"pony_name pony_" + classname;
+						}
+					}else if(curIndex < splitPoints[0]){
+						cls += (cls==""?"":" ")+"pony_timing";
 					}
-				}else if(curIndex < splitPoints[0]){
-					cls += (cls==""?"":" ")+"pony_timing";
-				}
-				textAndClassFunc(txt, cls);
-				curIndex += txtlen;
-			};
-		})();
+					textAndClassFunc(txt, cls);
+					curIndex += txtlen;
+				};
+			})();
 
-		return linestylefilter.textAndClassFuncSplitter(handleRegExpMatchsAfterSplit, splitPoints);
-	};
-}
+			return linestylefilter.textAndClassFuncSplitter(handleRegExpMatchsAfterSplit, splitPoints);
+		};
+	}
 
-function aceGetFilterStack(args) {
-	return [ponynames(args.linestylefilter)];
+	this.aceGetFilterStack = function(args) {
+		return [ponynames(args.linestylefilter)];
+	}
 }
 
 /* used on the client side only */
