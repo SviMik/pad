c1f62d46104d94f431370bb0426f3b78234c6cac
SviMik <svimik@mail.ru>
2015-05-16 13:57:22 +0300

    showImages plugin refactoring

--- etherpad/src/plugins/showImages/static/js/main.js
+++ etherpad/src/plugins/showImages/static/js/main.js
@@ -1,44 +1,67 @@
 function showImagesInit() {
-  this.hooks = ['aceGetFilterStack', 'aceCreateDomLine'];
-  this.aceGetFilterStack = aceGetFilterStack;
-  this.aceCreateDomLine = aceCreateDomLine;
+	this.hooks = ['aceGetFilterStack', 'aceCreateDomLine'];
+
+	var imagewas=-1;
+
+	function imagefilter(linestylefilter){
+		return function (lineText, textAndClassFunc) {
+			imagewas=-1;
+			var regExp = /http.+((\.png)|(\.jpg))/gi;
+			regExp.lastIndex = 0;
+			var splitPoints = [];
+			var execResult;
+			if(execResult = regExp.exec(lineText)){
+				var from = execResult.index;
+				var to = from+execResult[0].length;
+				splitPoints.push(from, to);
+			}else{
+				return textAndClassFunc;
+			}
+
+			var handleRegExpMatchsAfterSplit = (function() {
+				var curIndex = 0;
+				return function(txt, cls) {
+					var txtlen = txt.length;
+					if(curIndex >= splitPoints[0] && curIndex < splitPoints[1]){
+						var url=lineText.substring(splitPoints[0], splitPoints[1]);
+						cls += (cls==""?"":" ")+"image_"+curIndex+"_" + url;
+					}
+					textAndClassFunc(txt, cls);
+					curIndex += txtlen;
+				};
+			})();
+
+			return linestylefilter.textAndClassFuncSplitter(handleRegExpMatchsAfterSplit, splitPoints);
+		};
+	}
+
+	this.aceGetFilterStack = function(args) {
+		return [imagefilter(args.linestylefilter)];
+	}
+
+	this.aceCreateDomLine = function(args) {
+		if (args.cls.indexOf('image') > -1) {
+			var src=false;
+			var id=false;
+			cls = args.cls.replace(/(^| )image_([^_]+)_(\S+)/g, function(x0, space, id, image) {
+				id = id;
+				src = image;
+				return space + "image";
+			});
+
+			var tag='';
+			if(src!==false && imagewas!=id){
+				imagewas=id;
+				tag='<img src="' + src + '" style="max-width:500px"/>';
+			}
+
+			return [{
+				cls: cls,
+				extraOpenTags: tag,
+				extraCloseTags:''
+			}];
+		}
+	}
 }
 
-function aceGetFilterStack(args) {
-  return [
-//    args.linestylefilter.getRegexpFilter(
-//      new RegExp("#[^,#=!\\s][^,#=!\\s]*", "g"), 'padtag'),
-    args.linestylefilter.getRegexpFilter(
-      new RegExp("http.+((\.png)|(\.jpg))", "g"), 'image')
-  ];
-}
-
-function aceCreateDomLine(args) {
-  if (args.cls.indexOf('image') > -1) {
-    var src;
-    cls = args.cls.replace(/(^| )image:(\S+)/g, function(x0, space, image) {
-      src = image;
-      return space + "image image_" + image;
-    });
-
-   return [{
-   		cls: cls,
-   		extraOpenTags: '<img src="' + src + '" style="width:100%; max-width:500px;/>',
-		extraCloseTags:''
-   	}];
-  }
-  
-//   else if (args.cls.indexOf('padtag') >= 0) {
-//    var href;
-//    cls = args.cls.replace(/(^| )padtag:(\S+)/g, function(x0, space, padtag) {
-//      href = '/ep/tag/?query=' + padtag.substring(1);
-//      return space + "padtag padtag_" + padtag.substring(1);
-//    });
-//
-//   return [{
-//     cls: cls,
-//     extraOpenTags: '<strong><a href="' + href.replace(/\"/g, '&quot;') + '">',
-//     extraCloseTags: '</a></strong>'}];
-//  }
-}
 showImages = new showImagesInit();
