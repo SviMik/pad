549dce443dde30a748ef73e34f974d71ed3b8ca2
u0 <xzzv@users.noreply.github.com>
2015-05-29 23:14:01 +0300

    Use getTagFilter() in ponyNamesDecorator

--- etherpad/src/plugins/ponyNamesDecorator/static/js/main.js
+++ etherpad/src/plugins/ponyNamesDecorator/static/js/main.js
@@ -5,43 +5,31 @@ function ponyNamesDecoratorInit() {
 		args.iframeHTML.push('\'<link rel="stylesheet" type="text/css" href="/static/css/plugins/ponyNamesDecorator/pad.css"/>\'');
 	}
 
-	function ponynames(linestylefilter){
-		return function (lineText, textAndClassFunc) {
+	function getPonyNamesFilter(linestylefilter){
+        return linestylefilter.getTagFilter(function(lineText) {
+            var tagPlacement = {splitPoints: [], tagNames: []};
 			var regExp = /^([^a-z#]{0,18})(([a-z][a-z0-9]* *){1,3}:)/gi;
 			regExp.lastIndex = 0;
-			var splitPoints = [];
 			var execResult;
 			if(execResult = regExp.exec(lineText)){
 				var from = execResult.index + execResult[1].length;
 				var to = from+execResult[2].length;
-				splitPoints.push(from, to);
-			}else{
-				return textAndClassFunc;
+                if(lineText.substring(to, to+2) != "//"){ // prevents links from highlighting
+                    var classname = execResult[1].replace(/\s|:/g, '').toLowerCase();
+                    tagPlacement.splitPoints.push(from, to);
+                    tagPlacement.tagNames.push("pony_name pony_" + classname);
+                    if(from>0) {
+                        tagPlacement.splitPoints.push(0, from);
+                        tagPlacement.tagNames.push("pony_timing");
+                    }
+                }
 			}
-
-			var handleRegExpMatchsAfterSplit = (function() {
-				var curIndex = 0;
-				return function(txt, cls) {
-					var txtlen = txt.length;
-					if(curIndex >= splitPoints[0] && curIndex < splitPoints[1]){
-						if(lineText.substring(splitPoints[1], splitPoints[1]+2)!="//"){ // prevents links from highlighting
-							var classname=lineText.substring(splitPoints[0], splitPoints[1]).replace(/\s|:/g, '').toLowerCase();
-							cls += (cls==""?"":" ")+"pony_name pony_" + classname;
-						}
-					}else if(curIndex < splitPoints[0]){
-						cls += (cls==""?"":" ")+"pony_timing";
-					}
-					textAndClassFunc(txt, cls);
-					curIndex += txtlen;
-				};
-			})();
-
-			return linestylefilter.textAndClassFuncSplitter(handleRegExpMatchsAfterSplit, splitPoints);
-		};
+            return tagPlacement;
+        });
 	}
 
 	this.aceGetFilterStack = function(args) {
-		return [ponynames(args.linestylefilter)];
+		return [getPonyNamesFilter(args.linestylefilter)];
 	}
 }
 
