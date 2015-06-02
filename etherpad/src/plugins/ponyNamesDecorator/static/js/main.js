bd93708ad277633ab819c1b46dc7c67927d0cd62
u0 <xzzv@users.noreply.github.com>
2015-06-02 23:16:38 +0300

    Fix name detection and bolding in ponyNamesDecorator

--- etherpad/src/plugins/ponyNamesDecorator/static/js/main.js
+++ etherpad/src/plugins/ponyNamesDecorator/static/js/main.js
@@ -15,7 +15,7 @@ function ponyNamesDecoratorInit() {
 				var from = execResult.index + execResult[1].length;
 				var to = from+execResult[2].length;
                 if(lineText.substring(to, to+2) != "//"){ // prevents links from highlighting
-                    var classname = execResult[1].replace(/\s|:/g, '').toLowerCase();
+                    var classname = execResult[2].replace(/\s|:/g, '').toLowerCase();
                     tagPlacement.splitPoints.push(from, to);
                     tagPlacement.tagNames.push("pony_name pony_" + classname);
                     if(from>0) {
