3361acd79c4349597fe01d9679437f6f04c6c341
u0 <xzzv@users.noreply.github.com>
2015-07-04 12:58:54 +0300

    subChecker: use helpers.includeCss to include the CSS file
    To make browsers reload it from the server when it's updated.

--- etherpad/src/plugins/subChecker/hooks.js
+++ etherpad/src/plugins/subChecker/hooks.js
@@ -2,7 +2,9 @@ import("etherpad.log");
 import("faststatic");
 import("etherpad.utils.*");
 import("etherpad.globals.*");
+import("etherpad.helpers");
 
 function editBarItemsLeftPad(arg) {
+    helpers.includeCss('plugins/subChecker/pad.css');
     return arg.template.include('subCheckerButton.ejs', undefined, ['subChecker']);
 }
