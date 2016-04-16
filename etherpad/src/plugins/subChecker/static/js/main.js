1a700152cd335eca74805e3b14276d91c53d95b8
SviMik <svimik@mail.ru>
2016-04-16 03:05:29 +0300

    subChecker: length analysis

--- etherpad/src/plugins/subChecker/static/js/main.js
+++ etherpad/src/plugins/subChecker/static/js/main.js
@@ -16,6 +16,7 @@
 				<option value=10 id=c_error_10>Errors</option>\
 				<option value=5 id=c_error_5>Warnings</option>\
 				<option value=2 id=c_error_2>Info</option>\
+				<option value=1 id=c_error_1>Length</option>\
 			</select>\
 		<span style="margin-left: 30px"/>\
 			Download: \
@@ -46,12 +47,18 @@
 
 	this.check = function(){
 		if(!pad_parse()){
-
 			g("checker_list").innerHTML="pad_parse() error";
 			return;
 		}
-
 		var error_level=g("c_error_level") ? parseInt(g("c_error_level").value) : 0;
+		if(error_level==1){
+			check_length();
+		}else{
+			check_errors(error_level);
+		}
+	}
+
+	function check_errors(error_level){
 		var errors = find_errors();
 		var cnt=0;
 		var res = '<table id="subs_errors" style="border-spacing:0px">\
@@ -121,10 +128,10 @@
 			return false; // no pad - no parsing
 	
 		var padHtml = padeditor.ace.getFormattedCode();
-		if(padHtml==this.pad_prev_data)
+		if(padHtml==subChecker.pad_prev_data)
 			return true;
 		
-		this.pad_prev_data=padHtml;
+		subChecker.pad_prev_data=padHtml;
 	
 		var padTextLines = padeditor.ace.exportText().replace(/&nbsp;/g, ' ').replace(/\{\\[bi][01]\}/g, ' ').replace(/[ ]{2,}/g, ' ').split("\n");
 		var padHtmlLines = padHtml.replace(/&nbsp;/g, ' ').replace(/[ ]{2,}/g, ' ').replace(/<b> /g, ' <b>').split("\n");
@@ -152,8 +159,8 @@
 				if(textMatch == null) {
 					textMatch = text.match(/^([^а-яА-ЯёЁ\u2192]+)(.*)$/);
 				}
-				text_en = textMatch[1].replace(/\[[^\[\]]+\]/g, '').trim();
-				text_ru = textMatch[2].replace(/\[[^\[\]]+\]/g, '').replace(/^\u2192/, '').trim();
+				text_en = textMatch[1].replace(/\[[^\[\]]+\]/g, '').replace(/\\N/gi, "\n").trim();
+				text_ru = textMatch[2].replace(/\[[^\[\]]+\]/g, '').replace(/^\u2192/, '').replace(/\\N/gi, "\n").trim();
 				if(text_en || text_ru) {
 					if(name == "Auto" || name == "Multilang"){
 						var text_both = (text_en+" "+text_ru).trim();
@@ -164,7 +171,7 @@
 				}
 			}
 		}
-		this.subs = new_subs;
+		subChecker.subs = new_subs;
 		return true;
 	}
 
@@ -185,13 +192,13 @@
 	
 		var t_prev_end=0;
 
-		for(k in subs){
-			var line = subs[k][0];
-			var t = subs[k][1];
-			var l = subs[k][2];
-			var name = subs[k][3];
-			for (lang in subs[k][4]) {
-				var text = subs[k][4][lang].replace(/\\N/gi, "\n");
+		for(k in subChecker.subs){
+			var line = subChecker.subs[k][0];
+			var t = subChecker.subs[k][1];
+			var l = subChecker.subs[k][2];
+			var name = subChecker.subs[k][3];
+			for (lang in subChecker.subs[k][4]) {
+				var text = subChecker.subs[k][4][lang];
 
 				if (lang == 2) { // HTML checks
 					if (text.match(/<i>/))
@@ -245,7 +252,199 @@
 	
 		return errors;
 	}
-	
+
+	function pbar(a, b){
+		var max_width=185;
+		if(Math.max(a, b)>max_width){
+			var mul=max_width/Math.max(a, b);
+			a*=mul;
+			b*=mul;
+		}
+		a=Math.round(a);
+		b=Math.round(b);
+		if(b<=a){
+			return '<span class=pbar style="width:'+a+'px"><span class=green style="width:'+b+'px"></span></span>';
+		}else{
+			return '<span class=pbar><span class=green style="width:'+a+'px"></span><span class=red style="width:'+(b-a)+'px"></span></span>';
+		}
+	}
+
+	function check_length(){
+		var res = '<table id="subs_errors" style="border-spacing:0px">\
+				<thead>\
+					<tr>\
+						<th width=40 align=left>Line</th>\
+						<th width=40 align=left>EN</th>\
+						<th width=40 align=left>RU</th>\
+						<th width=190>Слоги</th>\
+						<th width=190>Буквы</th>\
+						<th width=30 align=left>CPS</th>\
+					</tr>\
+				</thead>\
+				<tbody>';
+
+		var whiteTableRow = false;
+		for(k in subChecker.subs){
+			var line = subChecker.subs[k][0];
+			var dur = subChecker.subs[k][2];
+			var en=subChecker.subs[k][4][0];
+			var ru=subChecker.subs[k][4][1];
+			line = line+1-subChecker.line_renumerator_offset;
+			var whiteTableRow = !whiteTableRow;
+			lineHtml = '<a href="javascript:subChecker.go_to_line('+line+');void(0)">'+line+'</a>';
+
+			if (ru.replace(/\s\-\s/g, ' ').match(/\+|-[^a-zа-яё]|[^a-zа-яё]-|^-|-$/i) || ru.match(/\{[^\\}][^}]*\}/) || ru.replace(/\{[^}]*\}/g, ' ').match(/[\/\\\(\)\[\]]/)){
+				res += '<tr style="background-color:'+(whiteTableRow?'white':'#e8e8e8')+'">\
+					<td style="padding:1px">'+lineHtml+'</td>\
+					<td style="padding:1px">'+c1+'</td>\
+					<td style="padding:1px;font-size:11px;color:#888">??</td>\
+					<td style="padding:1px;font-size:11px;color:#888;text-align:center" colspan=2>Строка с вариантами или комментариями.</td>\
+					<td>&nbsp;</td></tr>';
+				continue;
+			}
+
+			var cps=Math.floor(ru.replace(/[^a-zA-Zа-яА-ЯёЁ]/g, "").length/dur).toFixed(0);
+			var lines=(typeof(wordwrap)!="function" ? 0 : substr_count(wordwrap(ru.replace(/\\n/g, "\n").replace(/\\N/g, "\n").replace(/\n\n/g, "\n"), 58, "\n", false).trim(), "\n"))+1;
+			var c1=subChecker.syllable_count(en);
+			var c2=subChecker.syllable_count(ru);
+			var diff=c2-c1;
+			if(cps.length<2){cps="0"+cps;}
+			res += '<tr style="background-color:'+(whiteTableRow?'white':'#e8e8e8')+'">\
+				<td style="padding:1px">'+lineHtml+'</td>\
+				<td style="padding:1px">'+c1+'</td>\
+				<td style="padding:1px">'+c2+'</td>\
+				<td style="padding:1px">'+pbar(c1*4, c2*4)+(diff==0 ? '' : ' '+(diff>0?'+':'')+diff)+'</td>\
+				<td style="padding:1px'+(lines>2 ? ';background:#F00' : '')+'">'+pbar(en.length*1.9, ru.length*1.9)+'</td>\
+				<td style="padding:1px">'+(cps<20 ? cps : '<span style="background:#F00">'+cps+'</span>')+'</td></tr>';
+		}
+		res += '</tbody></table>';
+
+		g("checker_list").innerHTML=res;
+	}
+
+	/* Syllable counting functions */
+
+	function syllable_count_en_word(word) {
+		word = word.toLowerCase(); //word.downcase!
+		word = word.replace(/[^a-z']/g, '').trim(); // clean from punctuation marks, etc.
+		if(word.length < 1) { return 0; } // no word = no syllables
+		if(word.length <= 2) { return 1; } // 1 or 2 letter words have always 1 syllable... I guess...
+		word = word.replace(/in'$/, 'ing'); // just unroll for exception list
+		word = word.replace(/'s$/, 's'); // wrong counting on "there's", dirty hack - "theres" counts right
+		// Exception list
+		if(word=="something"){return 2;} // was 3
+		if(word=="someday"){return 2;} // was 3
+		if(word=="every"){return 2;} // was 3
+		if(word=="maybe"){return 2;} // was 1
+		// Main processing, found somewhere on the Internet. Works awfully.
+		word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, ''); //word.sub!(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '')
+		word = word.replace(/^y/, ''); //word.sub!(/^y/, '')
+		var m=word.match(/[aeiouy]{1,2}/g);
+		if(m==null){return 1;}
+		return m.length; //word.scan(/[aeiouy]{1,2}/).size
+	}
+
+	function syllable_count_ru_word(word) {
+		var vowel  = new String ('аеёиоуыэюя'); // Гласные буквы
+		var voiced = new String ('бвгджзлмнрхцчшщ'); // Звонкие и шипящие согласные
+		var deaf   = new String ('кпстф'); // Глухие согласные
+		var brief  = new String ('й'); // Й
+		var other  = new String ('ьъ'); // Другие
+		var cons   = new String ('бвгджзйклмнпрстфхцчшщ'); // Все согласные
+
+		word = word.toLowerCase(); //word.downcase!
+		var s = word.replace(/[^а-яё]/g, '').trim();
+		if(s.length < 1) { return 0; }
+
+		// Есть ли в строке гласные?
+		function isNotLastSep (remainStr) {
+			var is = false;
+			for (var i = 0; i < remainStr.length; i++) {
+				if (vowel.indexOf (remainStr.substr (i, 1)) != -1) { is = true; break; }
+			}
+			return is;
+		}
+
+		// Собственно функция разбиения слова на слоги
+		// Добавляем слог в массив и начинаем новый слог
+		function addSep () {
+			sepArr.push (tmpS);
+			tmpS = '';
+		}
+
+		var tmpL   = new String ();  // Текущий символ
+		var tmpS   = new String ();  // Текущий слог
+		var sepArr = new Array ();   // Массив слогов
+		for (var i = 0; i < s.length; i++) {
+			tmpL = s.substr (i, 1);
+			tmpS += tmpL;
+			// Проверка на признаки конца слогов
+			// если буква равна 'й' и она не первая и не последняя и это не последний слог
+			if (
+			  (i != 0) && 
+			  (i != s.length -1) && 
+			  (brief.indexOf (tmpL) != -1) &&
+			  (isNotLastSep (s.substr (i+1, s.length-i+1)))
+			) { addSep (); continue; }
+			// если текущая гласная и следующая тоже гласная
+			if (
+				(i < s.length - 1) && 
+				(vowel.indexOf (tmpL) != -1) && 
+				(vowel.indexOf (s.substr (i+1, 1)) != -1)
+			 ) { addSep (); continue; }
+			// если текущая гласная, следующая согласная, а после неё гласная
+			if (
+				(i < s.length - 2) && 
+				(vowel.indexOf (tmpL) != -1) && 
+				(cons.indexOf (s.substr (i+1, 1)) != -1) && 
+				(vowel.indexOf (s.substr (i+2, 1)) != -1)
+			 ) { addSep (); continue; }
+			// если текущая гласная, следующая глухая согласная, а после согласная и это не последний слог
+			if (
+				(i < s.length - 2) && 
+				(vowel.indexOf (tmpL) != -1) && 
+				(deaf.indexOf (s.substr (i+1, 1)) != -1) && 
+				(cons.indexOf (s.substr (i+2, 1)) != -1) &&
+				(isNotLastSep (s.substr (i+1, s.length-i+1)))
+			) { addSep (); continue; }
+			// если текущая звонкая или шипящая согласная, перед ней гласная, следующая не гласная и не другая, и это не последний слог
+			if (
+				(i > 0) && 
+				(i < s.length - 1) && 
+				(voiced.indexOf (tmpL) != -1) && 
+				(vowel.indexOf (s.substr (i-1, 1)) != -1) && 
+				(vowel.indexOf (s.substr (i+1, 1)) == -1) && 
+				(other.indexOf (s.substr (i+1, 1)) == -1) && 
+				(isNotLastSep (s.substr (i+1, s.length-i+1)))
+			) { addSep (); continue; }  
+			// если текущая другая, а следующая не гласная если это первый слог
+			if (
+				(i < s.length - 1) && 
+				(other.indexOf (tmpL) != -1) &&
+				((vowel.indexOf (s.substr (i+1, 1)) == -1) || 
+				(isNotLastSep (s.substr (0, i))))
+			) { addSep (); continue; } 
+		}
+		if(isNotLastSep(tmpS)){
+			sepArr.push (tmpS);
+		}
+		return sepArr.length;
+	}
+
+	this.syllable_count = function(str){
+		var words=str.replace(/&nbsp;/g, ' ').replace(/[ ]{2,}/g, ' ').split(' ');
+		var ret=0;
+		for(var i = 0; i < words.length; i++){
+			var word=words[i];
+			if(word.match(/[а-яА-ЯёЁ]/)){
+				ret+=syllable_count_ru_word(word);
+			}else if(word.match(/[a-zA-Z]/)){
+				ret+=syllable_count_en_word(word);
+			}
+		}
+		return ret;
+	}
+
 	function g(id) {
 		return document.getElementById(id);
 	}
